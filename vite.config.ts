0ba47c6d0fde0cb2610fc8ac50554cc905f8e8e4
cliz1 <clizbe@bu.edu>
2025-11-22 13:16:31 -0500

    fix vite config to include sound files in build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-  // change 'src' if your index.html lives somewhere else
   root: path.resolve(__dirname, "src"),
+
+  // explicitly tell Vite where 'public' lives
+  publicDir: path.resolve(__dirname, "public"),
 
   build: {
     // output to repo root/dist
     outDir: path.resolve(__dirname, "dist"),
-    emptyOutDir: true
-  }
+    emptyOutDir: true,
+  },
 });
