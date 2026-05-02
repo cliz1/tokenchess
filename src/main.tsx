fd6f21451d4b20cd18c7305bb3d63e7c3edc6f85
cliz1 <clizbe@bu.edu>
2026-05-02 18:51:14 -0400

    update global board theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import "./assets/chessground.jade.css"; // global board theme
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
